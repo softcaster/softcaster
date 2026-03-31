0173a58abdbe8cf696b17d009fea4fa3c14997a2
softcaster <87308287+softcaster@users.noreply.github.com>
2026-03-31 16:18:30 +0200

    Update NumberUtils.java
    Aggiunto metodo di comparazione

--- foundation/src/main/java/org/softcaster/commons/utils/NumberUtils.java
+++ foundation/src/main/java/org/softcaster/commons/utils/NumberUtils.java
@@ -17,6 +17,12 @@ public class NumberUtils {
         return _value.signum() == 0;
     }
     
+    public static boolean isGreaterThan(double value1, double value2) {
+        BigDecimal _value1 = BigDecimal.valueOf(value1);
+        BigDecimal _value2 = BigDecimal.valueOf(value2);
+        return _value1.compareTo(_value2) == 1;
+    }
+    
     public static <T extends Number> boolean isOdd(T number) {
         // L'operatore modulo (%) restituisce il resto della divisione
         // Usiamo longValue() per rendere il calcolo compatibile con tutti i Number
