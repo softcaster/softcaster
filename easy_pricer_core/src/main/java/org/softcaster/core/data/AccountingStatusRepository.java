b3c0e8f2c575fa51e15dd48955480912199b59c5
softcaster <softcaster@gmail.com>
2026-06-03 16:55:45 +0200

    preparazione db SOFIE

--- easy_pricer_core/src/main/java/org/softcaster/core/data/AccountingStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package org.softcaster.core.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountingStatusRepository extends JpaRepository<AccountingStatus, Integer> {
-
-    public AccountingStatus findByAccountingStatusId(Integer accountingStatusId);
-}
--- easy_pricer_core/src/main/java/org/softcaster/core/data/CurrencyPairRepository.java
+++
@@ -1,8 +0,0 @@
-package org.softcaster.core.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyPairRepository extends JpaRepository<CurrencyPair, Integer> {
-
-    public CurrencyPair findByIdCurrencyPair(Integer idCurrencyPair);
-}
