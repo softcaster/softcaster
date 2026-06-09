955f0130eb0b9b51e1e4ef0b39f94ffeb75b900b
softcaster <softcaster@gmail.com>
2026-06-09 23:15:45 +0200

    gestione state manager

--- easy_pricer_proc/src/main/java/org/softcaster/easy_pricer_proc/controllers/InternalSystem.java
+++ easy_pricer_proc/src/main/java/org/softcaster/easy_pricer_proc/controllers/InternalSystem.java
@@ -4,6 +4,10 @@
  */
 package org.softcaster.easy_pricer_proc.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.softcaster.easy_pricer_proc.services.EngineStateManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InternalSystem {
     
+    private static final Logger log = LoggerFactory.getLogger(InternalSystem.class);
+
+    @Autowired
+    private EngineStateManager stateManager;
+
     @PostMapping(value = "/internal/system/suspend")
     public ResponseEntity<String> suspend() {
-        System.out.println("Service suspended...");
+        // Cambia lo stato centralizzato
+        stateManager.suspend();
+        log.info("Service suspended...");
         return new ResponseEntity<>("Service suspended", HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "/internal/system/resume")
+    public ResponseEntity<String> resume() {
+        // Cambia lo stato centralizzato
+        stateManager.resume();
+        log.info("Service resumed...");
+        return new ResponseEntity<>("Service resumed", HttpStatus.OK);
     }
 }
