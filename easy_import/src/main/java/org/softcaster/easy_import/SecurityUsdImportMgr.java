477619a96d01c2b4cf15373791ea275599f02cd0
softcaster <softcaster@gmail.com>
2026-06-11 16:05:16 +0200

    easy_import per bond USD

--- easy_import/src/main/java/org/softcaster/easy_import/SecurityUsdImportMgr.java
+++ easy_import/src/main/java/org/softcaster/easy_import/SecurityUsdImportMgr.java
@@ -4,6 +4,9 @@
  */
 package org.softcaster.easy_import;
 
+import org.softcaster.provider.bondblox.BondBloxProvider;
+import org.softcaster.provider.bondblox.RefDatum;
+import org.softcaster.provider.enums.Market;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +18,11 @@ public class SecurityUsdImportMgr implements IImportMgr {
 
     @Override
     public void start(IProgressInfo progressInfo) {
+        BondBloxProvider provider = BondBloxProvider.getInstance();
+        RefDatum value = provider.getRefDatum("US912810QL52", Market.BONDS);
+        System.out.println(value.coupon);
+        System.out.println(value.issueDate);
+        System.out.println(value.maturityDate);
     }
 
     @Override
