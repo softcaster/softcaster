07f922ae514dd1acbbe21e0bf43ca805caf1e95f
softcaster <softcaster@gmail.com>
2026-09-18 12:20:58 +0200

    clean cmd_futures

--- ddl/sofie_rev.sql
+++ ddl/sofie_rev.sql
@@ -272,3 +272,8 @@ ALTER TABLE yield_curve_item  add CONSTRAINT fk_node_type FOREIGN KEY (node_type
 INSERT INTO gl_accounts (account_id, parent, code, description, is_postable,  statement_type, nature, balance)
 VALUES(nextval('gl_accounts_s'),21, '520020', 'Interest Expense - Sovereign Debt (Coupons)', TRUE, 2,5,1);
 --FDBM@IT@Base@POWER@F@202609
+
+delete from instrument_quote where master_data=65;
+delete from cmd_future_master_data where id_master_data=65;
+delete from future_master_data where id_master_data=65;
+delete from master_data where id_master_data=65;
