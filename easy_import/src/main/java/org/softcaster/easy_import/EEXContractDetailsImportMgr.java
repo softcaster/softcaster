447eede8d3e9a5ca0a2fd6f4663b56ef61f1e7e4
softcaster <softcaster@gmail.com>
2026-09-23 20:28:24 +0200

    import contract details eex

--- easy_import/src/main/java/org/softcaster/easy_import/EEXContractDetailsImportMgr.java
+++ easy_import/src/main/java/org/softcaster/easy_import/EEXContractDetailsImportMgr.java
@@ -9,22 +9,33 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.softcaster.commons.utils.LoggerMgr;
 import org.softcaster.commons.xml.ParamsMgr;
+import org.softcaster.easy_import.beans.EexColumnsMapping;
+import org.softcaster.easy_import.beans.EexContractRow;
 import org.springframework.stereotype.Service;
 
 @Service("EEX Contract Details")
 public class EEXContractDetailsImportMgr implements IImportMgr {
 
+    private EexColumnsMapping ecm = null;
+    private IProgressInfo progressInfo = null;
+
     @Override
     public void start(IProgressInfo progressInfo) {
+        this.progressInfo = progressInfo;
         ParamsMgr paramsMgr = ParamsMgr.getInstance();
         String fileName = paramsMgr.getParamValue("EEX_CONTR_DETAILS");
         Path file = Paths.get(IMPORT_PATH + "/" + fileName);
@@ -33,16 +44,41 @@ public class EEXContractDetailsImportMgr implements IImportMgr {
 
             for (Sheet sheet : workbook) {
 
-                System.out.println("SHEET: " + sheet.getSheetName());
-
-                Iterator<Row> rows = sheet.iterator();
-
-                if (!rows.hasNext()) {
-                    continue;
+                int headerIndex = findHeaderRow(sheet);
+                Row headerRow = sheet.getRow(headerIndex);
+                if (headerRow == null) {
+                    throw new IllegalStateException("No header found");
                 }
 
-                // Header
-                Row headerRow = rows.next();
+                ecm = new EexColumnsMapping(headerRow);
+                List<EexContractRow> eexContractRowList = new ArrayList<>();
+
+                int total = sheet.getLastRowNum();
+                int current = 0;
+
+                for (int r = headerIndex + 1;
+                        r <= sheet.getLastRowNum();
+                        r++) {
+
+                    Row row = sheet.getRow(r);
+
+                    if (row == null) {
+                        continue;
+                    }
+
+                    // parse
+                    if (!parseRow(row, ecm, eexContractRowList)) {
+                        System.out.println("Error reading line: " + r);
+                    }
+
+                    if (this.progressInfo != null) {
+                        int percent = (int) ((current / (double) total) * 100);
+                        this.progressInfo.updateProgress("Importing " + r + " (" + current + "/" + total + ")", percent);
+                    }
+
+                }
+                int size = eexContractRowList.size();
+                System.out.println(size);
             }
         } catch (IOException ex) {
             LoggerMgr.logError(ex.getLocalizedMessage());
@@ -51,6 +87,81 @@ public class EEXContractDetailsImportMgr implements IImportMgr {
 
     @Override
     public void terminate() {
+        if (progressInfo != null) {
+             progressInfo.updateProgress("End Importing ", 100);
+        }
     }
 
+    private int findHeaderRow(Sheet sheet) {
+
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                String value = cell.toString().trim();
+                if ("PRODUCT_ID".equalsIgnoreCase(value)) {
+                    return row.getRowNum();
+                }
+            }
+        }
+
+        throw new IllegalStateException(
+                "Header PRODUCT_ID not found"
+        );
+    }
+
+    private boolean parseRow(Row row, EexColumnsMapping ecm, List<EexContractRow> eexContractRowList) {
+
+        Cell cell;
+        EexContractRow eexContractRow = new EexContractRow();
+
+        int index = ecm.getColumnIndex("PRODUCT_ID");
+        cell = row.getCell(index);
+        if (cell == null) {
+            return false;
+        }
+        eexContractRow.setProductId(cell.getStringCellValue());
+
+        index = ecm.getColumnIndex("EXPIRY_YEAR");
+        cell = row.getCell(index);
+        if (cell == null) {
+            return false;
+        }
+        eexContractRow.setExpiryYear((int) cell.getNumericCellValue());
+
+        index = ecm.getColumnIndex("EXPIRY_MONTH");
+        cell = row.getCell(index);
+        if (cell == null) {
+            return false;
+        }
+        eexContractRow.setExpiryMonth((int) cell.getNumericCellValue());
+
+        index = ecm.getColumnIndex("FIRST_TRADING_DATE");
+        cell = row.getCell(index);
+        if (cell == null) {
+            return false;
+        }
+        java.util.Date rawDate = cell.getDateCellValue();
+        java.time.LocalDate dt = LocalDate.ofInstant(rawDate.toInstant(), ZoneId.systemDefault());
+        eexContractRow.setFirstTradingDate(dt);
+
+        index = ecm.getColumnIndex("LAST_TRADING_DATE");
+        cell = row.getCell(index);
+        if (cell == null) {
+            return false;
+        }
+        rawDate = cell.getDateCellValue();
+        dt = LocalDate.ofInstant(rawDate.toInstant(), ZoneId.systemDefault());
+        eexContractRow.setFirstTradingDate(dt);
+
+        index = ecm.getColumnIndex("EXPIRY_DATE");
+        cell = row.getCell(index);
+        if (cell == null) {
+            return false;
+        }
+        rawDate = cell.getDateCellValue();
+        dt = LocalDate.ofInstant(rawDate.toInstant(), ZoneId.systemDefault());
+        eexContractRow.setExpiryDate(dt);
+
+        eexContractRowList.add(eexContractRow);
+        return true;
+    }
 }
